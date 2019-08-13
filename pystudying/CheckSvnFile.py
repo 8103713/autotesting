073a4d3572c2f9964fcd101cead344b17b7f0b55
likai <8103713@qq.com>
2019-08-13 17:07:16 +0800

    svncheck

--- pystudying/CheckSvnFile.py
+++ pystudying/CheckSvnFile.py
@@ -19,7 +19,9 @@ db_creatTime = '0'
 dev_en = '0'
 test_en = '0'
 pro_en = '0'
-remark = '0'
+remark1 = '0'
+remark2 = '0'
+remark3 = '0'
 
 o_address = '172.16.9.106'
 n_address = '172.18.238.62'
@@ -51,13 +53,13 @@ elif f_select == 41:
 	db_table = "project_report_fy19_研发项目1"
 
 #写数据库
-def db_sql(db_dept,db_projectId,db_projectName,db_scm,sub_name,db_creatTime,filename,tj,status,dev_en,test_en,pro_en,remark):
+def db_sql(db_dept,db_projectId,db_projectName,db_scm,sub_name,db_creatTime,filename,tj,status,dev_en,remark1,test_en,remark2,pro_en,remark3):
 	db = pymysql.connect('localhost', 'likai_qec', 'stq@456', 'likai')  # 建立数据库连接
 	cursor = db.cursor()  # 使用 cursor() 方法创建一个游标对象 cursor
 	# SQL 插入语句
-	sql = "INSERT INTO project_report(id,dept,projectid,projectname,scm,number,projecttime,item,state,url,deven,testen,proen,remark) \
+	sql = "INSERT INTO project_report(id,dept,projectid,projectname,scm,number,projecttime,item,state,url,deven,remark1,testen,remark2,proen,remark3) \
 	       VALUES ("+"null," + "'" + db_dept + "','" +db_projectId + "','" + db_projectName + "','"+ db_scm + "','" + sub_name + "','" \
-		  + db_creatTime + "','" + filename + "','" + tj + "','" + status+"','" + dev_en+"','" + test_en+"','" + pro_en+"','" + remark+"')"
+		  + db_creatTime + "','" + filename + "','" + tj + "','" + status+"','" + dev_en+"','" + remark1+"','" + test_en+"','" + remark2+"','" + pro_en+"','" + remark3+"')"
 	sql1 = sql.replace("project_report", db_table)
 #	try:
 		# 执行sql语句
@@ -91,17 +93,17 @@ def svn_it(filename,sub_name):
 		status = status1
 		tj = "已提交"
 		print(tj+":"+status)
-		db_sql(db_dept,db_projectId,db_projectName,db_scm,sub_name,db_creatTime,filename,tj,status,dev_en,test_en,pro_en,remark)
+		db_sql(db_dept,db_projectId,db_projectName,db_scm,sub_name,db_creatTime,filename,tj,status,dev_en,remark1,test_en,remark2,pro_en,remark3)
 	elif (status2 != 0):
 		status = status2
 		tj = "已提交"
 		print(tj + ":  " + status)
-		db_sql(db_dept, db_projectId, db_projectName, db_scm, sub_name, db_creatTime, filename, tj, status,dev_en,test_en,pro_en,remark)
+		db_sql(db_dept, db_projectId, db_projectName, db_scm, sub_name, db_creatTime, filename, tj, status,dev_en,remark1,test_en,remark2,pro_en,remark3)
 	else:
 		status = " "
 		tj = "未提交"
 		print(tj + ":" + status)
-		db_sql(db_dept, db_projectId, db_projectName, db_scm, sub_name, db_creatTime, filename, tj, status,dev_en,test_en,pro_en,remark)
+		db_sql(db_dept, db_projectId, db_projectName, db_scm, sub_name, db_creatTime, filename, tj, status,dev_en,remark1,test_en,remark2,pro_en,remark3)
 
 
 
@@ -148,31 +150,31 @@ def deal_excel(file_txt):
 	develop_index = 0
 	test_index = 0
 	product_index = 0
-	global dev_en,test_en,pro_en,remark
+	global dev_en,test_en,pro_en,remark1,remark2,remark3
 	for j in range(1,sheet2.nrows):
 		sheet2_content = sheet2.cell(j,0).value
 		if sheet2_content == "开发环境":
 			#develop_index = j
 			dev_en = sheet2.cell(j,1).value
 			dev_en = dev_en.replace("\n","。")
-			remark = sheet2.cell(j,2).value
+			remark1 = sheet2.cell(j,2).value
 			#remark = remark.replace("\n","。")
 			print("开发环境：",dev_en)
 
 		elif sheet2_content == "测试环境":
 			test_en = sheet2.cell(j, 1).value
 			test_en = test_en.replace("\n", "。")
-			remark = sheet2.cell(j,2).value
+			remark2 = sheet2.cell(j,2).value
 			#remark = remark.replace("\n","。")
 			#test_index = j
 
 		elif sheet2_content == "生产环境" or sheet2_content == "部署环境" or sheet2_content == "生产（部署）环境":
 			pro_en = sheet2.cell(j, 1).value
 			pro_en = pro_en.replace("\n", "。")
-			remark = sheet2.cell(j,2).value
+			remark3 = sheet2.cell(j,2).value
 			#remark = remark.replace("\n","。")
 			#product_index = j
-			
+
 
 
 
